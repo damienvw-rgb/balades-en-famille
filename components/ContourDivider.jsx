dee11b8a908e8ec30064e4bb2397ea753c0fb2bb
damienvw-rgb <damienvw@gmail.com>
2026-08-06 20:53:55 +0200

    Delete components directory

--- components/ContourDivider.jsx
+++
@@ -1,8 +0,0 @@
-export default function ContourDivider() {
-  return (
-    <svg className="contour" viewBox="0 0 1080 28" preserveAspectRatio="none" aria-hidden="true">
-      <path d="M0,14 Q60,2 120,14 T240,14 T360,14 T480,14 T600,14 T720,14 T840,14 T960,14 T1080,14" fill="none" stroke="#8fae87" strokeWidth="1.5" />
-      <path d="M0,20 Q60,10 120,20 T240,20 T360,20 T480,20 T600,20 T720,20 T840,20 T960,20 T1080,20" fill="none" stroke="#c1542d" strokeWidth="1" opacity="0.6" />
-    </svg>
-  );
-}
